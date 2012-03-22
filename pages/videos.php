3b87c26010c41f8ff6916fbf425451280405a4e1
Romaric Pighetti <pighetti@W143.(none)>
2012-03-22 21:49:16 +0100

    videos page
    add the videos page with 5 videos

---
+++ pages/videos.php
@@ -0,0 +1,70 @@
+<div class="span9">
+	
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="alert alert-info">
+				<p>Interview de déficients visuels lors de la journée DeViNT 2008</p>
+				<div align="center">
+					<iframe width="640" height="480" src="http://www.youtube.com/embed/FSiC3by9iRU" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row-fluid"> 
+		
+	</div>
+	
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="alert alert-info">
+				<p>Un jeu de snowboard présenté à la journée DeViNT 2008</p>
+				<div align="center">
+					<iframe width="640" height="480" src="http://www.youtube.com/embed/Ah32VgZKmQw" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="alert alert-info">
+				<p>Le stand des projets DeViNT en 2008</p>
+				<div align="center">
+					<iframe width="640" height="480" src="http://www.youtube.com/embed/45a09Z-L9qI" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row-fluid"> 
+		
+	</div>
+	
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="alert alert-info">
+				<p>Reportage de france 3 en 2007</p>
+				<div align="center">
+					<iframe width="640" height="480" src="http://www.youtube.com/embed/MwxyvofD5eA" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row-fluid"> 
+		
+	</div>
+	
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="alert alert-info">
+				<p>Reportage de france 3 en 2005</p>
+				<div align="center">
+					<iframe width="640" height="480" src="http://www.youtube.com/embed/TVOnmgf4iLI" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+				 
+</div>
