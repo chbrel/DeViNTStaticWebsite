f9d21444e5bde55d592fab4219583b7d0502ac4f
Christian Brel <ch.brel@gmail.com>
2012-03-22 19:14:33 +0100

    Edited about and home pages

--- pages/about.php
+++ pages/about.php
@@ -6,7 +6,7 @@
 
   		<div class="row-fluid">
   			<div class="span8 offset2">
-				<p>Depuis huit ans, chaque année, les élèves ingénieurs informaticiens de Polytech Nice Sophia inventent des logiciels destinés aux utilisateurs mal-voyants et non voyants des ordinateurs.</p>
+				<p>Depuis dix ans, chaque année, les élèves ingénieurs informaticiens de Polytech Nice Sophia inventent des logiciels destinés aux utilisateurs mal-voyants et non voyants des ordinateurs.</p>
 <p>En pratique, ces logiciels sont développés en très grande majorité dans le cadre des projets DeViNT (DeViNT signifie Déficients Visuels et Nouvelles Technologies). Ils sont ensuite distribués gratuitement,  sur les CD DeViNT pour certains, et sur ce site pour tous les autres, puis qu’il en existe maintenant plus de cent, avec leur synthèse vocale VocalyzeSIVOX.</p>
 <p>La formule pédagogique des projets DeViNT est originale, et n’a pas d’équivalent à notre connaissance. Elle semble extrêmement formatrice et intéressante pour les élèves ingénieurs, de l’avis de tous.</p>
 <p>Dans la forme actuelle, les projets DeViNT durent 12 semaines successives, au second semestre de l’année universitaire, en première année de spécialisation en Sciences Informatiques (troisième année d’école). Au semestre précédent, les élèves ont appris pour certains, et approfondi pour d’autres, le langage java.</p>
@@ -25,6 +25,26 @@
 		<li>ils introduisent la problématique de l’accessibilité, puisque c’est une contrainte forte des utilisateurs</li>
 		<li>ils forment à respecter des contraintes pour l’intégration des projets sur le CD et sa diffusion sur le site</li>
 		<li>ils offrent la satisfaction d’inventer et de créer de A à Z avec les technologies enseignées et en cohérence des contraintes fortes, un projet susceptible d’être intégré dans la famille des projets DeViNT.</li>
+	</ul>
+</p>
+<p>
+	<h2>Success story des projets DeViNT</h2>
+	<ul>
+		<li>La synthèse vocale a été présentée aux XXVIème "Journées d'Etudes sur la Parole" en juin 2006 (publication: "Une synthèse vocale destinée aux déficients visuels", <em> Hélène Collavizza, Jean-Paul Stromboni</em>, XXVIèmes Journées d'Etude sur la Parole, 12-16 juin 2006, Dinard).</li>
+		<li>Le CD-ROM 2007 ainsi que la synthèse vocale ont concouru  au  "Challenge Handicap Inter-universitaire de Metz" édition 2007. Le groupe d'élèves qui nous a représentés a remporté le premier prix de la Communication autour de l'ordinateur.</li>
+		<li>Le CD-ROM 2007  et la synthèse vocale ont fait l'objet d'un atelier à la journée «World Usability Day at Sophia-Antipolis» le 29 novembre 2007 (<a href="http://worldusabilityday.org/world-usability-day-sophia-antipolis-france"> http://worldusabilityday.org/world-usability-day-sophia-antipolis-france </a>).</li>
+		<li>Les projets DeViNT ont été présentés par Virginie Lauer et Christian Brel à "Intellicore Tech Talks" à Sophia le 24 Juin 2008 (voir <a href="http://www.dailymotion.com/video/x5wm6z_devint-deficients-visuels-et-nouvel_tech"> http://www.dailymotion.com/video/x5wm6z_devint-deficients-visuels-et-nouvel_tech </a>).</li>
+		<li>Le projet  qui a remporté le ``Prix DeViNT'' 2008 a été présenté à l'émission ``La tête au carré'' sur France Inter le 
+		12 Juin 2008. <br>
+		Anne-Marie Hugues (organisatrice du prix), Jean l'Herbon de Lussats (coprési\-dent de la journée) et les élèves concepteurs du jeu ont été les invités de Mathieu Vidard pour parler des projets DeViNT et du jeu gagnant : un simulateur de ski réalisé grâce à une planche de skateboard.</li>
+		<li>Les projets DeViNT ont été présentés dans une table ronde et dans un atelier par Sébastien Mosser et Christian Brel, tous deux élèves de Polytech Nice Sophia, aux neuvièmes "Rencontres Mondiales du Logiciel Libre" à Mont-de-Marsan en juillet 2008.</li>
+		<li> Les projets et la journée DeViNT ont été présentés au salon <em>autonomic</em> les 2 et 3 Décembre 2011 à Nice.</li>
+		<li> Les projets DeViNT ont été présentés dans la revue <em>Voir demain</em>,trimestriel, avril/juin 2011, n°443 (page 22).</li>
+		<li>Les projets DeViNT ont été présentés dans<em> L'écho du PASIE - Innover et Expérimenter </em> (bulletin d'innovation et d'expérimentation pédagogique de l'Académie de Nice en avril 2011)</li>
+		<li>Ils apparaissent dans le livret national <em>L'innovation: une histoire contemporaine du changement en éducation</em> par François Muller, du DRDIE (Département Recherche et de Développement en Innovation en Enseignement) du Ministère de l'éducation Nationale, édité par le CNDP en février 2012, dans la rubrique TICE.</li>
+		<li>Les projets DeViNT sont sélectionnés pour le Boulevard des innovations, lors des <em>Journées de l'innovation</em>, Palais de l'UNESCO à Paris, 28 et 29 mars 2012</li>
+		<li>Cinq projets DeViNT 2011 sont candidats au concours Jeux Vidéo 2012 en marge de la conférence Handicap 2012 à l'ENSAM à Paris en juin 2012</li>
+		<li>à compléter ...</li>
 	</ul>
 </p>
 			</div>
--- pages/home.php
+++ pages/home.php
@@ -6,6 +6,17 @@
 
   		<div class="row-fluid">
   			<div class="span8 offset2">
-				<p>Un bon pti blabla d'accueil avec un finish du style "Nous sommes les meilleurs!" :)</p>
+				<p>DeViNT est l'acronyme de Déficients Visuels et Nouvelles Technologies (noter l'emploi des  majuscules). Les projets du même nom sont nés il y a dix ans avec la journée DeViNT organisée d'abord par l'ESSI (Ecole Supérieure de Sciences Informatiques) puis par Polytech'Nice-Sophia en collaboration avec l'INRIA, le laboratoire I3S, et plusieurs associations, dont l'APEDV 06 (Association des parents d'élèves déficients visuels des Alpes Maritimes).</p>
+<p>Après trois ans, les projets DeViNT se sont enrichis d'une   collaboration avec  l'Institut Clément ADER, et l'école du Château, à Nice, deux établissements spécialisés qui accueillent des élèves handicapés, entre autres mal voyants et non voyants.  Depuis, nous allons à Nice en cours de projet pour collaborer avec les utilisateurs potentiels des projets, et les élèves et enseignants de ces instituts spécialisés viennent à Sophia voir les projets finis lors de la journée DeViNT. </p>
+<p>Depuis 2006, les projets aboutis sont intégrés sur un CD qui est distribué gratuitement aux visiteurs de la journée, et à tous ceux qui  en font la demande.</p>
+<p>En 2007, un site a été créé pour distribuer les projets DeViNT sur internet, qui devrait être mis à jour dés que possible, c'est <a href="http://projets.polytech.unice.fr/Devint">http://projets.polytech.unice.fr/Devint</a>.</p>
+<p>Dés le départ, une synthèse vocale SIVOX, puis VocalyzeSIVOX, a été développée entre autres sous la forme de projets SI4, pour donner une voix gratuite aux projets, entre autres sur le CD. Cette synthèse vocale utilise le moteur MBROLA de l'équipe du professeur Thierry Dutoit de l'Université Polytechnique de Mons en Belgique.</p>
+<p>En 2011, le CD qui fonctionnait sous Windows XP a été étendu à Windows Seven, et l'installateur des projets pour le CD a été codé en langage java. De plus, des licences GPL ont été posées sur les projets et sur le CD, en plus de la licence MBROLA qui prohibe les utilisations commerciale ou militaires.</p>
+<p>Les projets DeViNT, environ vingt nouveaux projets par an, sont donc à mettre au crédit des élèves ingénieurs du département Sciences Informatiques de  Polytech Nice Sophia, des enseignants chercheurs qui les encadrent, et des élèves, professeurs et personnels de l'Institut Clément Ader et de l'école du Château à Nice. Ils permettent aux élèves ingénieurs :</p>
+<ul>
+  <li>de découvrir la problématique du handicap visuel, </li>
+  <li>de rencontrer un public utilisateur déficient visuel,</li>
+  <li>de mener un projet logiciel à terme, en collaboration avec ses utilisateurs</li>
+</ul>
 			</div>
 		</div>
