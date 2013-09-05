18452cf4b0bbdf9f07cf1b09bcac559671696314
Christian Brel <ch.brel@gmail.com>
2013-09-05 23:18:29 +0200

    Updated cddevint.php to add new way to install CD DeViNT (from a reduce version of CD by adding zip version of projects).

--- pages/cddevint.php
+++ pages/cddevint.php
@@ -7,6 +7,51 @@
   		<div class="row-fluid">
   			<div class="span8 offset2">
   				<p>
+  				Depuis 2013, la procédure de téléchargement du CD DeViNT a été modifiée. Voici en quelques mots les étapes à suivre pour une installation correct du CD DeViNT,
+  				et plus précisément, du CD DeViNT que vous constituez vous même!
+  				</p>
+  				
+  				<h2>Comment installer les projets DeViNT</h2>
+  				<p>
+  					L'installation des projets DeViNT consiste en deux étapes.
+  					<ul>
+  						<li>
+  							<strong>Première étape : télécharger le CD DeViNT minimal</strong>
+  							<br/><br/>
+  							<p>Il s'agit de télécharger un dossier compressé au format zip dont la taille est de 273 méga-octets. Dans ce dossier que
+  								l'on doit d'abord décompresser, on trouvera un sous dossier nommé 'LesLogiciels' destiné à recevoir tous les projets DeViNT
+  								que vous voulez installer (et que vous téléchargerez dans la seconde étape), ainsi que le fichier 'installation.bat' dont
+  								l'exécution permet d'installer et de désinstaller les projets téléchargés afin de pouvoir les utiliser.<br/>
+								Initialement, le dossier 'LesLogiciels' contient un seul projet, le projet Rapidmot. Pour installer Rapidmot,
+								il suffit de double cliquer sur 'installation.bat' et de suivre la procédure d'installation. Pour exécuter Rapidmot,
+								on utilisera l'icône ‘Jeux DeViNT' créée sur le bureau par l'installation. Notez l'icône AideDeViNT qui fournit une aide
+								sur Rapidmot. Notez que la synthèse vocale SIVOX a été installée également comme un projet.
+								<p>
+									<a href="./cddevint/minimal/CDDeViNTminimal.zip" class="btn btn-primary"><i class="icon-eye-open icon-white"></i> Cliquez ici pour télécharger le CD DeViNT minimal (au format zip, moins de 300 Mo)</a>
+								</p>
+							</p>
+  						</li>
+  						<li>
+  							<strong>Seconde étape (répétée autant de fois que vous voulez) :</strong>
+  							<br/><br/>
+  							<p>Vous devez ensuite télécharger un par un, éventuellement en plusieurs fois, les projets DeViNT qui vous intéressent,
+  								que vous trouverez sur la page des projets ci-dessous. Il suffit de placer chacun des projets que vous téléchargez,
+  								tel quel, sans le décompresser, dans le dossier 'LesLogiciels'. Exécutez ensuite 'installation.bat' permet d'installer
+  								(ou de désinstaller) sélectivement tous les projets qui se trouvent dans le dossier 'Les Logiciels'.
+								<p>
+									<a href="./index.php?action=projets" class="btn btn-info"><i class="icon-flag icon-white"></i> Cliquer ici pour vous rendre à la page des projets.</a>
+								</p>
+							</p>
+						</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+  		
+  		<div class="row-fluid">
+  			<div class="span8 offset2">
+  				<h2>CD DeViNT 2012</h2>
+  				<p>
   					Le CD DeViNT 2012 regroupe une sélection de projets créés à l'occasion des journées DeViNT 2004, 2005, ... et 2012.
   					<br /><br />
 					Si vous téléchargez le CD DeVinT 2012 ci-dessous, vous devrez d'abord le décompresser.<br/>
@@ -15,13 +60,15 @@
 				<p>
 					<a href="./cddevint/2012/CDDeViNT2012.zip" class="btn btn-primary"><i class="icon-eye-open icon-white"></i> Cliquer ici pour télécharger le CD DeViNT 2012 (au format zip, moins de 400 Mo)</a>
 				</p>
-				<p><a href="./cddevint/2012/livretCDDeViNT2012.pdf" class="btn btn-info"><i class="icon-flag icon-white"></i> Cliquer ici pour télécharger le livret du CD 2012 au format pdf.</a>
+				<p>
+					<a href="./cddevint/2012/livretCDDeViNT2012.pdf" class="btn btn-info"><i class="icon-flag icon-white"></i> Cliquer ici pour télécharger le livret du CD 2012 au format pdf.</a>
 				</p>
   			</div>
 		</div>
 
   		<div class="row-fluid">
   			<div class="span8 offset2">
+  				<h2>CD DeViNT 2011</h2>
   				<p>
   					Le CD DeViNT 2011 regroupe une sélection de projets créés à l'occasion des journées DeViNT 2004, 2005, ... et 2011.
   					<br /><br />
