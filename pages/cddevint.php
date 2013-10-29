e30a6638fc2508491b151f23772654361daa6f89
Christian Brel <ch.brel@gmail.com>
2013-10-29 21:54:16 +0100

    Updated CDDeViNT download process.

--- pages/cddevint.php
+++ pages/cddevint.php
@@ -34,10 +34,7 @@
   						<li>
   							<strong>Seconde étape (répétée autant de fois que vous voulez) :</strong>
   							<br/><br/>
-  							<p>Vous devez ensuite télécharger un par un, éventuellement en plusieurs fois, les projets DeViNT qui vous intéressent,
-  								que vous trouverez sur la page des projets ci-dessous. Il suffit de placer chacun des projets que vous téléchargez,
-  								tel quel, sans le décompresser, dans le dossier 'LesLogiciels'. Exécutez ensuite 'installation.bat' permet d'installer
-  								(ou de désinstaller) sélectivement tous les projets qui se trouvent dans le dossier 'Les Logiciels'.
+  							<p>Vous pouvez ensuite télécharger les projets DeViNT qui vous intéressent depuis la page des projets ci-dessous. Décompressez les projets téléchargés dans le dossier 'LesLogiciels' du CD DeViNT minimal. Exécutez enfin le fichier 'installation.bat' pour installer (ou désinstaller) les projets qui se trouvent dans le dossier 'Les Logiciels'.
 								<p>
 									<a href="./index.php?action=projets" class="btn btn-info"><i class="icon-flag icon-white"></i> Cliquer ici pour vous rendre à la page des projets.</a>
 								</p>
@@ -46,42 +43,4 @@
 					</ul>
 				</p>
 			</div>
-		</div>
-  		
-  		<div class="row-fluid">
-  			<div class="span8 offset2">
-  				<h2>CD DeViNT 2012</h2>
-  				<p>
-  					Le CD DeViNT 2012 regroupe une sélection de projets créés à l'occasion des journées DeViNT 2004, 2005, ... et 2012.
-  					<br /><br />
-					Si vous téléchargez le CD DeVinT 2012 ci-dessous, vous devrez d'abord le décompresser.<br/>
-					Voir le livret du CD 2012 pour plus d'informations.<br />
-				</p>
-				<p>
-					<a href="./cddevint/2012/CDDeViNT2012.zip" class="btn btn-primary"><i class="icon-eye-open icon-white"></i> Cliquer ici pour télécharger le CD DeViNT 2012 (au format zip, moins de 400 Mo)</a>
-				</p>
-				<p>
-					<a href="./cddevint/2012/livretCDDeViNT2012.pdf" class="btn btn-info"><i class="icon-flag icon-white"></i> Cliquer ici pour télécharger le livret du CD 2012 au format pdf.</a>
-				</p>
-  			</div>
-		</div>
-
-  		<div class="row-fluid">
-  			<div class="span8 offset2">
-  				<h2>CD DeViNT 2011</h2>
-  				<p>
-  					Le CD DeViNT 2011 regroupe une sélection de projets créés à l'occasion des journées DeViNT 2004, 2005, ... et 2011.
-  					<br /><br />
-					Si vous téléchargez le CD DeVinT 2011 ci-dessous, vous devrez d'abord le décompresser.<br/>
-					Ensuite, pour l'installer  sous Windows XP, ou Windows Seven 32 ou 64 bits, il suffit de le mettre dans le lecteur de CD, ou bien d'exécuter le fichier autorun.bat, (ou encore le fichier Install.exe). <br />
-					Le CD installe Java Runtime Environment (JRE 1.6) dans le dossier jre.<br />
-					Ensuite, vous pouvez installer et désinstaller sélectivement les projets du CD, la synthèse vocale est installée automatiquement.<br />
-					Un raccourci est placé sur le bureau, qui permet de sélectionner le projet à exécuter dans la liste des projets installés. Un autre raccourci permet d'accéder à l'aide sur les projets. Voir également le livret du CD 2011 pour plus d'informations.<br />
-				</p>
-				<p>
-					<a href="./cddevint/2011/CDDeViNT2011.zip" class="btn btn-primary"><i class="icon-eye-open icon-white"></i> Cliquer ici pour télécharger le CD DeViNT 2011 (au format zip, moins de 400 Mo)</a>
-				</p>
-				<p><a href="./cddevint/2011/livretCDDeViNT2011.pdf" class="btn btn-info"><i class="icon-flag icon-white"></i> Cliquer ici pour télécharger le livret du CD 2011 au format pdf.</a>
-				</p>
-  			</div>
 		</div>
